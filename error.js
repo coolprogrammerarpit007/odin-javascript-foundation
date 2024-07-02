25f0b11bbd40ae30cb38801db01fcf9a6e38cf04
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-02 18:24:41 +0530

    common errors in programming

---
+++ error.js
@@ -0,0 +1,21 @@
+`use strict`;
+
+// Understanding Errors in Programming
+
+// common type of errors 
+// 1. Syntax Error
+// 2. Refrence Error
+// 3. Type Error
+
+
+// syntax error -> error occurs when you do not follow the correct rules of a programming language.
+// Refrence Error -> This error occurs when variable does not exist in scope.
+// Type Error -> This error occurs when a method is applied over in-compitable data
+
+// try{
+//     let name = prompt("Enter Name: ");
+// }
+
+// catch(e){
+//     console.log(e);
+// }
